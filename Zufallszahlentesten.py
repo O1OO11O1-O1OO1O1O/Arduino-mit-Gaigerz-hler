172d97de5206c6a345d11e51fa8ea490c3958289
O1OO11O1-O1OO1O1O <marten.jacobsen@gmx.de>
2025-01-24 15:18:36 +0100

    Update Zufallszahlentesten.py

--- Zufallszahlentesten.py
+++ Zufallszahlentesten.py
@@ -2,10 +2,12 @@ werte = input("Werte?")
 haeufigkeiten = {}
 eingabeWerte = [werte]
 eingabeWerte = werte.split(",")
-print("Konvertierte Liste:", eingabeWerte)
-print(len(eingabeWerte))
+durchschnit = 0
+
+print(len(eingabeWerte), "Zeichen")
 
 for x in eingabeWerte:
+    durchschnit = durchschnit +int(x)
 
     if x in haeufigkeiten:
         haeufigkeiten[x] += 1
@@ -15,4 +17,6 @@ for x in eingabeWerte:
 print(haeufigkeiten)
 
 for zeichen, anzahl in haeufigkeiten.items():
-    print(f" '{zeichen}' : {anzahl} mal")
+    print(f" '{zeichen}' : {anzahl} mal", 100 /len(eingabeWerte) *anzahl, "%")
+
+print("Durchschnit:", durchschnit /len(eingabeWerte))
